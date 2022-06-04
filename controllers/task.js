fbc2d82004bab0d006b1ecf507ad39746a0359a6
KaranShah-2511 <karansshah25@gmail.com>
2022-06-05 00:20:05 +0530

    add crud

--- controllers/task.js
+++ controllers/task.js
@@ -1,21 +1,61 @@
-
+import Task from "../models/Task.js"
 
 class Tasks {
-    static getAllTasks = (req, res) => {
-        res.send('Get All Tasks')
+    static getAllTasks = async (req, res) => {
+        try {
+            const getAllTasks = await Task.find()
+            res.status(200).json({ getAllTasks })
+
+        } catch (error) {
+            res.status(500).json({ error })
+        }
+        // res.send('Get All Tasks')
 
     }
-    static createTask = (req, res) => {
-        res.send('Create Task')
+    static createTask = async (req, res) => {
+
+        try {
+            const task = await Task.create(req.body)
+            res.status(200).json({ task })
+
+        } catch (error) {
+            res.status(500).json({ error })
+
+        }
+
     }
-    static updateTask = (req, res) => {
-        res.send('Update Task')
+    static updateTask = async (req, res) => {
+        // res.send('Update Task')
+        try {
+            const updateTask = await Task.findByIdAndUpdate(req.params.id, req.body, { new: true })
+            res.status(200).json({ updateTask })
+
+        } catch (error) {
+            res.status(500).json({ error })
+        }
     }
-    static deleteTask = (req, res) => {
-        res.send('Delete Task')
+    static deleteTask = async (req, res) => {
+        try {
+            const deleteTask = await Task.findByIdAndDelete(req.params.id)
+            res.json({ deleteTask })
+
+        } catch (error) {
+            res.status(500).json({ error })
+        }
+
+
+        // res.send('Delete Task')
     }
-    static getTaskById = (req, res) => {
-        res.send('Get Task By Id')
+    static getTaskById = async (req, res) => {
+        try {
+            const task = await Task.findById(req.params.id)
+            res.json({ task })
+
+        } catch (error) {
+            res.status(500).json({ error })
+        }
+
+        // res.send('Get Task By Id')
     }
 
 
---
+++ models/Task.js
@@ -0,0 +1,16 @@
+// import mongoose from 'mongoose ';
+import mongoose from 'mongoose';
+
+const TaskSchema = new mongoose.Schema({
+    firstName: {
+        type: String,
+        required: true
+    },
+    lastName: {
+        type: String,
+        required: true
+    },
+})
+
+const Task = mongoose.model('Task', TaskSchema);
+export default Task
