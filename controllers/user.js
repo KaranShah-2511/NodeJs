d439c9c93babd0ae25c7a8ce06a6ac5ac0a51ef4
KaranShah-2511 <60110585+KaranShah-2511@users.noreply.github.com>
2022-09-14 14:03:49 +0530

    sign_in function

--- controllers/user.js
+++ controllers/user.js
@@ -1,24 +1,38 @@
 import User from "../models/User.js"
 import asyncWrapper from "../middleware/async.js"
 import jwt from "jsonwebtoken"
-import  bcrypt from "bcrypt"
+import bcrypt from "bcrypt"
 
 
 class Users {
     static register = asyncWrapper(async (req, res) => {
         var newUser = new User(req.body);
         newUser.hash_password = bcrypt.hashSync(req.body.password, 10);
-        newUser.save(function(err, user) {
-          if (err) {
-            return res.status(400).send({
-              message: err
-            });
-          } else {
-            user.hash_password = undefined;
-            return res.json(user);
-          }
+        newUser.save(function (err, user) {
+            if (err) {
+                return res.status(400).send({
+                    message: err
+                });
+            } else {
+                user.hash_password = undefined;
+                return res.json(user);
+            }
         });
     })
+
+    static sign_in = asyncWrapper(async (req, res) => {
+        User.findOne({
+            email: req.body.email
+        }, function (err, user) {
+            if (err) throw err;
+            if (!user || !user.comparePassword(req.body.password)) {
+                return res.status(401).json({ message: 'Authentication failed. Invalid user or password.' });
+            }
+            console.log('user', user);
+            return res.json({ token: jwt.sign({ email: user.email, fullName: user.fullName, _id: user._id }, 'RESTFULAPIs') });
+        });
+    })
+
 }
 
 export default Users;
--- routes/user.js
+++ routes/user.js
@@ -3,6 +3,7 @@ const router = express.Router();
 import Users from "../controllers/user.js";
 
 router.post('/register', Users.register );
+router.post('/sign_in', Users.sign_in );
 
 
 
